2ee6ce3b3b67be57cbd49fa0ff3be6e642d5382b
msakurad <danieldarthez@proton.me>
2023-03-31 11:11:56 -0300

    ex02

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msakurad <msakurad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 11:01:19 by msakurad          #+#    #+#             */
+/*   Updated: 2023/03/31 11:11:05 by msakurad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = argc;
+	if (argc < 1)
+		return (0);
+	while (--i > 0)
+	{
+		ft_putstr(argv[i]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
