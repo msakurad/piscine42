2b4b3ba6bd9e4404884ba62f82a76742d862f917
msakurad <danieldarthez@proton.me>
2023-03-31 10:56:27 -0300

    ex01, completo

--- c06/ex01/ft_print_params.c
+++ c06/ex01/ft_print_params.c
@@ -3,19 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_params.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msakurad <msakurad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joshua <joshua@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 16:08:58 by msakurad          #+#    #+#             */
-/*   Updated: 2023/03/23 16:14:34 by msakurad         ###   ########.fr       */
+/*   Updated: 2023/03/31 10:52:07 by joshua           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc < 1)
 		return (0);
-	while
+	argv++;
+	while (*argv)
+	{
+		ft_putstr(*argv);
+		write(1, "\n", 1);
+		argv++;
+	}
 	return (0);
 }
